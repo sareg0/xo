55da25f8443fe74689dde6a2a9f488f7e2e51503
Sara personal <sara_personal@Saras-Work-MacBook-Pro.local>
2022-02-05 10:21:22 +0100

    Create addMove and createBoard functions

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,6 +1,33 @@
-import {tictactoe} from './tictactoe'
+import {createBoard, addMove} from './tictactoe'
+
+const board = {
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null,
+  7: null,
+  8: null,
+  9: null,
+}
 
 test('creates empty board', () => {
-  const thing = tictactoe()
-  expect(thing).toBe(3)
+  const thing = createBoard()
+  expect(board).toEqual(board)
+})
+
+test('makes a move', () => {
+  const thing = addMove('x', 1)
+  expect(thing).toEqual({
+    1: 'x',
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+    6: null,
+    7: null,
+    8: null,
+    9: null,
+  })
 })
--- tictactoe.ts
+++ tictactoe.ts
@@ -1,3 +1,35 @@
-export function tictactoe() {
-  return 3
+// Board
+
+type Piece = "x" | "y"
+type Board = {
+  1: null | Piece,
+  2: null | Piece,
+  3: null | Piece,
+  4: null | Piece,
+  5: null | Piece,
+  6: null | Piece,
+  7: null | Piece,
+  8: null | Piece,
+  9: null | Piece,
+}
+
+let board: Board = {
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null,
+  7: null,
+  8: null,
+  9: null,
+}
+
+export function createBoard() {
+  return board
+}
+
+export function addMove(symbol: Piece, position: keyof Board) {
+  board["1"] = 'x'
+  return board
 }
