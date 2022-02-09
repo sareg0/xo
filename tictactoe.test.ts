b170eb8ce110049cd35b6098ac9966669e60a792
Sara personal <sara_personal@Saras-Work-MacBook-Pro.local>
2022-02-09 08:35:55 +0100

    Add a test to create checks for wins

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,6 +1,6 @@
-import {createBoard, addMove} from './tictactoe'
-
-const board = {
+import {createBoard, addMove, checkForWin, Piece, Board} from './tictactoe'
+ 
+const emptyBoard: Board = {
   1: null,
   2: null,
   3: null,
@@ -12,9 +12,47 @@ const board = {
   9: null,
 }
 
+const verticalWin: Board = {
+  1: "x",
+  2: null,
+  3: null,
+  4: "x",
+  5: null,
+  6: null,
+  7: "x",
+  8: null,
+  9: null,
+}
+
+// const horizontalWin = {
+//   1: "o",
+//   2: "o",
+//   3: "o",
+//   4: null,
+//   5: null,
+//   6: null,
+//   7: null,
+//   8: null,
+//   9: null,
+// }
+
+// const diagonalWin = {
+//   1: null,
+//   2: null,
+//   3: "o",
+//   4: null,
+//   5: "o",
+//   6: null,
+//   7: "o",
+//   8: null,
+//   9: null,
+// }
+
+
+
 test('creates empty board', () => {
   const thing = createBoard()
-  expect(board).toEqual(board)
+  expect(thing).toEqual(emptyBoard)
 })
 
 test('makes a move', () => {
@@ -31,3 +69,12 @@ test('makes a move', () => {
     9: null,
   })
 })
+
+test('checks for win', () => {
+  const thing = checkForWin(verticalWin)
+  // winning symbol
+  // type of win
+  // winning positions
+  expect(thing).toStrictEqual(["1","4","7"])
+})
+
--- tictactoe.ts
+++ tictactoe.ts
@@ -1,7 +1,7 @@
 // Board
 
-type Piece = "x" | "y"
-type Board = {
+export type Piece = "x" | "y"
+export type Board = {
   1: null | Piece,
   2: null | Piece,
   3: null | Piece,
@@ -30,6 +30,37 @@ export function createBoard() {
 }
 
 export function addMove(symbol: Piece, position: keyof Board) {
-  board["1"] = 'x'
+  board[position] = symbol
   return board
+}
+ 
+export function checkForWin(board: Board) {
+  // return Object.entries(board).reduce(([key, value], accumulator) => {
+    // Check all vertical possibilities for the value
+    const verticalWin = checkForVerticalWin(board["1"], 1)
+    if (verticalWin) {
+      return verticalWin
+    }
+    // return accumulator.push(verticalWin)
+  // }, [])
+  // position 1, check the vertical
+}
+
+
+//Stopping point
+//The board is the one with only one x on it. 
+//So we are using the board we altered in move. 
+//When we run the test, the board is the one we changed during hte previous 
+//test to have one move on it. 
+//What we need is the board to be a) passed in as an argument or b) have the 
+// board be controlled separately? try both see what fits.
+
+function checkForVerticalWin (symbol: Piece, position: keyof Board) {
+  if ([1,4,7].includes(position)) {
+    console.log("&&&", board["1"], board["4"], board["7"], board)
+    if (board["1"] === symbol && board["4"] === symbol && board["7"] === symbol) {
+      // console.log('**checkForVerticalWin**', symbol, position)
+      return ["1", "4", "7"]
+    }
+  }
 }
