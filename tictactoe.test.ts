bfb86589aba88a6eb28dc434673c666421e54a30
Sara Regan <regan.sara@gmail.com>
2022-02-25 09:25:11 +0100

    Use Object.create to create a newGame with board as a property

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,4 +1,4 @@
-import {createBoard, addMove, checkForWin, Piece, Board} from './tictactoe'
+import {newGame, Board} from './tictactoe'
  
 const emptyBoard: Board = {
   1: null,
@@ -12,180 +12,185 @@ const emptyBoard: Board = {
   9: null,
 }
 
-const verticalWinOne: Board = {
-  1: "x",
-  2: null,
-  3: null,
-  4: "x",
-  5: null,
-  6: null,
-  7: "x",
-  8: null,
-  9: null,
-}
-const verticalWinTwo: Board = {
-  1: "x",
-  2: "o",
-  3: null,
-  4: "x",
-  5: "o",
-  6: null,
-  7: null,
-  8: "o",
-  9: null,
-}
+// const verticalWinOne: Board = {
+//   1: "x",
+//   2: null,
+//   3: null,
+//   4: "x",
+//   5: null,
+//   6: null,
+//   7: "x",
+//   8: null,
+//   9: null,
+// }
+// const verticalWinTwo: Board = {
+//   1: "x",
+//   2: "o",
+//   3: null,
+//   4: "x",
+//   5: "o",
+//   6: null,
+//   7: null,
+//   8: "o",
+//   9: null,
+// }
 
-const verticalWinThree: Board = {
-  1: "x",
-  2: null,
-  3: "x",
-  4: "x",
-  5: "o",
-  6: "x",
-  7: "o",
-  8: "o",
-  9: "x",
-}
+// const verticalWinThree: Board = {
+//   1: "x",
+//   2: null,
+//   3: "x",
+//   4: "x",
+//   5: "o",
+//   6: "x",
+//   7: "o",
+//   8: "o",
+//   9: "x",
+// }
 
-const diagonalWinOne: Board = {
-  1: "x",
-  2: null,
-  3: null,
-  4: "x",
-  5: "x",
-  6: null,
-  7: "o",
-  8: null,
-  9: "x",
-}
+// const diagonalWinOne: Board = {
+//   1: "x",
+//   2: null,
+//   3: null,
+//   4: "x",
+//   5: "x",
+//   6: null,
+//   7: "o",
+//   8: null,
+//   9: "x",
+// }
 
-const diagonalWinTwo: Board = {
-  1: "x",
-  2: null,
-  3: "o",
-  4: "x",
-  5: "o",
-  6: null,
-  7: "o",
-  8: null,
-  9: "x",
-}
+// const diagonalWinTwo: Board = {
+//   1: "x",
+//   2: null,
+//   3: "o",
+//   4: "x",
+//   5: "o",
+//   6: null,
+//   7: "o",
+//   8: null,
+//   9: "x",
+// }
 
-const horizontalWinOne: Board = {
-  1: "o",
-  2: "o",
-  3: "o",
-  4: null,
-  5: null,
-  6: null,
-  7: null,
-  8: null,
-  9: null,
-}
+// const horizontalWinOne: Board = {
+//   1: "o",
+//   2: "o",
+//   3: "o",
+//   4: null,
+//   5: null,
+//   6: null,
+//   7: null,
+//   8: null,
+//   9: null,
+// }
 
-const horizontalWinTwo: Board = {
-  1: "o",
-  2: "o",
-  3: null,
-  4: "x",
-  5: "x",
-  6: "x",
-  7: null,
-  8: null,
-  9: null,
-}
-const horizontalWinThree: Board = {
-  1: "o",
-  2: "o",
-  3: null,
-  4: null,
-  5: null,
-  6: null,
-  7: "o",
-  8: "o",
-  9: "o",
-}
+// const horizontalWinTwo: Board = {
+//   1: "o",
+//   2: "o",
+//   3: null,
+//   4: "x",
+//   5: "x",
+//   6: "x",
+//   7: null,
+//   8: null,
+//   9: null,
+// }
+// const horizontalWinThree: Board = {
+//   1: "o",
+//   2: "o",
+//   3: null,
+//   4: null,
+//   5: null,
+//   6: null,
+//   7: "o",
+//   8: "o",
+//   9: "o",
+// }
 
+describe('newGame', () => {
+  test('it can create multiple new games', () => {
+    const gameOne = newGame()
+    const gameTwo = newGame()
+    expect(gameOne.board === gameTwo.board).toBe(false)
+    expect(gameOne.board).toEqual(emptyBoard)
+    expect(gameTwo.board).toEqual(emptyBoard)
+  })
 
-test('creates empty board', () => {
-  const thing = createBoard()
-  expect(thing).toEqual(emptyBoard)
-})
-
-describe('addMove', () => {
-  it('adds a move to the board', () => {
-    const board = createBoard()
-    addMove('x', 1, board)
-    expect(board).toEqual({
-      1: 'x',
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-      8: null,
-      9: null,
+  describe('addMove', () => {
+    test('it can make a move to valid positions', () => {
+      const gameOne = newGame()
+      const gameTwo = newGame()
+      gameOne.addMove('x', 3)
+      expect(gameOne.board).toEqual({
+        1: null,
+        2: null,
+        3: "x",
+        4: null,
+        5: null,
+        6: null,
+        7: null,
+        8: null,
+        9: null,
+      })
+  
+      gameTwo.addMove('x', 8)
+      expect(gameTwo.board).toEqual({
+        1: null,
+        2: null,
+        3: null,
+        4: null,
+        5: null,
+        6: null,
+        7: null,
+        8: "x",
+        9: null,
+      })
     })
-    addMove('o', 2, board)
-    expect(board).toEqual({
-      1: 'x',
-      2: 'o',
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-      8: null,
-      9: null,
+    test('it cannot make a move to an invalid position', () => {
+      const gameOne = newGame()
+      expect(() => gameOne.addMove('x', 15)).toThrow()
+      expect(gameOne.board).toEqual(emptyBoard)
+    })
+    test('it cannot make a move with an invalid piece', () => {
+      const gameOne = newGame()
+      expect(() => gameOne.addMove('*', 3)).toThrow()
+      expect(gameOne.board).toEqual(emptyBoard)
     })
   })
 })
 
 
-
-describe('checkForWin', () => {
-  it('checks for vertical win', () => {
-    const winOne = checkForWin(verticalWinOne)
-    const winTwo = checkForWin(verticalWinTwo)
-    const winThree = checkForWin(verticalWinThree)
+// describe('checkForWin', () => {
+//   it('checks for vertical win', () => {
+//     const winOne = checkForWin(verticalWinOne)
+//     const winTwo = checkForWin(verticalWinTwo)
+//     const winThree = checkForWin(verticalWinThree)
   
-    expect(winOne).toStrictEqual(["1","4","7"])
-    expect(winTwo).toStrictEqual(["2","5","8"])
-    expect(winThree).toStrictEqual(["3","6","9"])
-  })
-  it('checks for diagonal win', () => {
-    const winOne = checkForWin(diagonalWinOne)
-    const winTwo = checkForWin(diagonalWinTwo)
+//     expect(winOne).toStrictEqual(["1","4","7"])
+//     expect(winTwo).toStrictEqual(["2","5","8"])
+//     expect(winThree).toStrictEqual(["3","6","9"])
+//   })
+//   it('checks for diagonal win', () => {
+//     const winOne = checkForWin(diagonalWinOne)
+//     const winTwo = checkForWin(diagonalWinTwo)
   
-    expect(winOne).toStrictEqual(["1","5","9"])
-    expect(winTwo).toStrictEqual(["3","5","7"])
-  })
-  it('checks for horizontal win', () => {
-    const winOne = checkForWin(horizontalWinOne)
-    const winTwo = checkForWin(horizontalWinTwo)
-    const winThree = checkForWin(horizontalWinThree)
+//     expect(winOne).toStrictEqual(["1","5","9"])
+//     expect(winTwo).toStrictEqual(["3","5","7"])
+//   })
+//   it('checks for horizontal win', () => {
+//     const winOne = checkForWin(horizontalWinOne)
+//     const winTwo = checkForWin(horizontalWinTwo)
+//     const winThree = checkForWin(horizontalWinThree)
   
-    expect(winOne).toStrictEqual(["1","2","3"])
-    expect(winTwo).toStrictEqual(["4","5","6"])
-    expect(winThree).toStrictEqual(["7","8","9"])
-  })
-})
+//     expect(winOne).toStrictEqual(["1","2","3"])
+//     expect(winTwo).toStrictEqual(["4","5","6"])
+//     expect(winThree).toStrictEqual(["7","8","9"])
+//   })
+// })
 
 
 
 
 
 
-
-
-//What happens when someone ties?
-//Is a tie in noughts and crosses possible?
-//Not in a game where the first to get a streak wins
-
-//How might this game work when you want to say, connect any number
-//of position on a board of any size?
-// e.g. connect four. Could this also be used to do connect 4,
-// with some minor changes.
-
-//How might I organise the tests better?
+// //How might I organise the tests better?
+// //How might I test for the mistakes I have made?
--- tictactoe.ts
+++ tictactoe.ts
@@ -13,8 +13,10 @@ export type Board = {
   9: null | Piece,
 }
 
-export function createBoard(): Board {
-  return {
+// TODO: how does one type the game?
+export function newGame() {
+  const game = Object.create(boardFunctions)
+  game.board = {
     1: null,
     2: null,
     3: null,
@@ -25,68 +27,69 @@ export function createBoard(): Board {
     8: null,
     9: null,
   }
+  return game
 }
 
-export function addMove(symbol: Piece, position: keyof Board, board: Board) {
-  // && ['x','o'].includes(symbol
-  if (Object.keys(board).includes(position.toString())) {
-    board[position] = symbol
-  } else {
-    throw Error('that is an invalid move!!')
-  }
-  return board
-}
-
-// should we always run through the board, to find if there is a win? 
-// are there other ways, to narrow it down?
-// What should we do if there is a tie? Should ties be impossible?
-export function checkForWin (board: Board) {
-  const possibleWins = [
-    ["1", "2", "3"], //H
-    ["1", "4", "7"], //V
-    ["1", "5", "9"], //D
-    ["2", "5", "8"], //V
-    ["3", "6", "9"], //v
-    ["3", "5", "7"], //D
-    ["4", "5", "6"], //H
-    ["7", "8", "9"], //H
-  ]
-
-  let win = []
-  for (const possibleWin of possibleWins) {
-    if (win.length === 3) {
-      return win
+const boardFunctions = {
+  addMove: function(symbol: Piece, position: keyof Board) {
+    if (Object.keys(this.board).includes(position.toString()) && ['x', 'o'].includes(symbol)) {
+      this.board[position] = symbol
+    } else {
+      throw Error('that is an invalid move!!')
     }
-    win = possibleWin.reduce((accumulator, currentValue, currentIndex, array) => {
-      if (board[`${currentValue}`] === null) {
-        // When there is no value in the cell, reset the streak of moves.
-        // Possible improvement: only zero out the streak if there is a streak length
-        accumulator = []
-      } else {
-        if (currentIndex === 0) {
-          // If there is a symbol in the cell and it's the first position of our possible Win
-          // use it to start the streak
-          accumulator.push(currentValue)
-        } else {
-          // If the symbol of the current position is the same as the symbol at the last cell
-          // of our possilbe win, it's a streak, so add it to the streak
-          console.log(board[`${currentValue}`], board[`${array[currentIndex-1]}`])
-          if (board[`${currentValue}`] === board[`${array[currentIndex-1]}`]) {
-            // Hint: push is a mutator. You will mutate the accumulator here, so 
-            // you won't have it as it was before. Consider what other possibilities there
-            // are for returning a new value
-            accumulator.push(currentValue)
-          } else {
-            // If the symbol in the current and last position of the possible win are not
-            // the same, the streak is broken. 0 it out!
-            accumulator = []
-          }
-        }
-      }
-      console.log('acc', accumulator)
-      return accumulator
-    }, [])
   }
-  console.log('win', win)
-  return win
-}
+} 
+
+// // should we always run through the board, to find if there is a win? 
+// // are there other ways, to narrow it down?
+// // What should we do if there is a tie? Should ties be impossible?
+// export function checkForWin (board: Board) {
+//   const possibleWins = [
+//     ["1", "2", "3"], //H
+//     ["1", "4", "7"], //V
+//     ["1", "5", "9"], //D
+//     ["2", "5", "8"], //V
+//     ["3", "6", "9"], //v
+//     ["3", "5", "7"], //D
+//     ["4", "5", "6"], //H
+//     ["7", "8", "9"], //H
+//   ]
+
+//   let win = []
+//   for (const possibleWin of possibleWins) {
+//     if (win.length === 3) {
+//       return win
+//     }
+//     win = possibleWin.reduce((accumulator, currentValue, currentIndex, array) => {
+//       if (board[`${currentValue}`] === null) {
+//         // When there is no value in the cell, reset the streak of moves.
+//         // Possible improvement: only zero out the streak if there is a streak length
+//         accumulator = []
+//       } else {
+//         if (currentIndex === 0) {
+//           // If there is a symbol in the cell and it's the first position of our possible Win
+//           // use it to start the streak
+//           accumulator.push(currentValue)
+//         } else {
+//           // If the symbol of the current position is the same as the symbol at the last cell
+//           // of our possilbe win, it's a streak, so add it to the streak
+//           console.log(board[`${currentValue}`], board[`${array[currentIndex-1]}`])
+//           if (board[`${currentValue}`] === board[`${array[currentIndex-1]}`]) {
+//             // Hint: push is a mutator. You will mutate the accumulator here, so 
+//             // you won't have it as it was before. Consider what other possibilities there
+//             // are for returning a new value
+//             accumulator.push(currentValue)
+//           } else {
+//             // If the symbol in the current and last position of the possible win are not
+//             // the same, the streak is broken. 0 it out!
+//             accumulator = []
+//           }
+//         }
+//       }
+//       console.log('acc', accumulator)
+//       return accumulator
+//     }, [])
+//   }
+//   console.log('win', win)
+//   return win
+// }
